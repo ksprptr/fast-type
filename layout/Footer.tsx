42052639e9339b6a34337077d2936f74d79a71a6
dortwess <dortwess@gmail.com>
2023-07-08 21:47:00 +0200

    continuing

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className='page-width'>
         <div className='md:flex block text-center justify-between py-4 text-lg text-gray-50'>
           <h3>&copy; FastType 2023</h3>
-          <h3>Created by <Link href="https://dortwess.com" target='_blank' className='text-lime-500 hover:text-lime-600 duration-150 font-medium'>Petr Dortwess</Link></h3>
+          <h3>Created by <Link href="https://dortwess.com" target='_blank' className='text-lime-500 hover:text-lime-600 duration-150 font-medium'>Dortwess</Link></h3>
         </div>
       </div>
     </footer>
