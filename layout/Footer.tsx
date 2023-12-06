ccbca2bae1d871627c688d47c275acf794db30f6
Petr Kašpar <92054010+kasparrpetr@users.noreply.github.com>
2023-12-07 00:29:04 +0100

    updated design

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -3,21 +3,15 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800">
-      <div className="page-width">
-        <div className="md:flex block text-center justify-between py-4 text-lg text-gray-50">
-          <h3>&copy; FastType 2023</h3>
-          <h3>
-            Created by{" "}
-            <Link
-              href="https://kasparpetr.com"
-              target="_blank"
-              className="text-lime-500 hover:text-lime-600 duration-150 font-medium"
-            >
-              Petr Kašpar
-            </Link>
-          </h3>
-        </div>
+    <footer className="text-zic-50 text-center">
+      <div className="flex flex-col py-4 text-lg text-gray-50">
+        <h3>&copy; FastType 2023</h3>
+        <h3>
+          Created by{" "}
+          <Link href="https://kasparpetr.com" target="_blank" className="text-lime-500 hover:underline duration-150 font-medium">
+            Petr Kaspar
+          </Link>
+        </h3>
       </div>
     </footer>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,84 @@
 import fullWords from "../components/Arrays";
-import Image from "next/image";
 import TypeWriter from "typewriter-effect";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { Roboto_Mono } from "next/font/google";
 
-const roboto = Roboto_Mono({
-  weight: "600",
-  subsets: ["latin"],
-});
+type ActiveWords = {
+  prev: string;
+  current: string;
+  next: string;
+};
 
 export default function Home() {
-  const [textToWrite, setTextToWrite] = useState<string[]>([]);
-  const [writedText, setWritedText] = useState<string[]>([]);
-  const [typed, setTyped] = useState<boolean>(false);
+  const [ready, setReady] = useState<boolean>(false);
+  const [activeWords, setActiveWords] = useState<ActiveWords>({
+    prev: "predchozi",
+    current: "aktivni",
+    next: "nasledujici",
+  });
 
   return (
     <main className="max-w-screen-2xl px-8 mx-auto">
-      <section className="flex flex-col justify-center items-start h-screen">
-        <div className="flex jsutify-between items-center">
-          <div>
-            <motion.h1 initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }} className="text-8xl text-zinc-50 font-normal">
-              Welcome to
-            </motion.h1>
-            <motion.h1 initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.25 }} className="font-medium text-lime-500 text-10xl leading-none mt-4">
-              FastType
-            </motion.h1>
-            <motion.div initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.5 }} className="text-2xl text-gray-50 mt-6">
-              <TypeWriter
-                onInit={(tw) => {
-                  tw.pauseFor(600)
-                    .typeString("Get better experience of typing.")
-                    .callFunction(() => setTyped(true))
-                    .start();
-                }}
-                options={{
-                  delay: 50,
-                  deleteSpeed: 10,
-                  cursor: "",
-                }}
-              />
-            </motion.div>
-            <motion.button initial={{ opacity: 0, y: 100 }} animate={{ opacity: typed ? 1 : 0, y: typed ? 0 : 100 }} transition={{ duration: 0.5 }} className="border-2 border-lime-500 px-4 py-2 rounded-md text-lime-500 text-xl mt-6 hover:bg-lime-500 hover:border-lime-500 hover:text-zinc-50  shadows duration-150">
-              Start typing...
-            </motion.button>
-          </div>
-          <div>
-            <Image src={"/image.png"} alt={""} width={1000} height={1000} />
-          </div>
-        </div>
+      <section className="flex flex-col justify-center items-center h-screen">
+        <motion.h1 initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }} className="md:text-6xl text-4xl text-zinc-50 font-normal">
+          Welcome to
+        </motion.h1>
+        <motion.h1 initial={{ opacity: 0, x: -500 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.3, delay: 0.5 }} className="font-medium italic text-lime-500 md:text-10xl text-8xl leading-none mt-4">
+          FastType
+        </motion.h1>
+        <motion.div initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.75 }} className="md:text-2xl text-lg text-gray-50 mt-10">
+          <TypeWriter
+            onInit={(tw) => {
+              tw.pauseFor(1000)
+                .typeString("Smart typing application.")
+                .callFunction(() => setReady(true))
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(1000)
+                .typeString("Get better experience of typing.")
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(1000)
+                .typeString("Type without looking at keyboard.")
+                .pauseFor(1000)
+                .deleteAll()
+                .pauseFor(1000)
+                .typeString("It's simple and easy.")
+                .start();
+            }}
+            options={{
+              delay: 50,
+              deleteSpeed: 50,
+              loop: true,
+            }}
+          />
+        </motion.div>
+        <motion.button initial={{ opacity: 0, y: 100 }} animate={{ opacity: ready ? 1 : 0, y: ready ? 0 : 100 }} transition={{ duration: 0.5 }} onClick={() => document.getElementById("type")?.scrollIntoView()} className="border-2 border-lime-500 px-4 py-2 rounded-md text-lime-500 md:text-xl text-base mt-10 hover:bg-lime-500 hover:border-lime-500 hover:text-zinc-50 duration-150">
+          {"Let's start"}
+        </motion.button>
       </section>
-      <div className="bg-gray-600 rounded-lg my-16" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" data-aos-delay="200">
-        <div className="flex justify-between p-3 px-4 bg-gray-700 rounded-t-lg items-center">
-          <div className="flex gap-x-6 items-center">
-            <h3>
-              Words/min: <span className="text-lime-500">0</span>
-            </h3>
-            <h3>
-              Chars/min: <span className="text-lime-500">0</span>
-            </h3>
-            <h3>
-              Accuracy: <span className="text-lime-500">0%</span>
-            </h3>
+      {ready && (
+        <section id="type" className="flex flex-col justify-center items-center h-screen">
+          <ul className="flex gap-x-8 items-center font-medium text-3xl text-zinc-50 select-none">
+            <div className="text-center text-lime-500 md:text-xl text-base">
+              <li>Prev</li>
+              <li className="md:text-2xl text-lg text-zinc-400 mt-2">{activeWords.prev}</li>
+            </div>
+            <div className="text-center text-lime-500 md:text-xl text-base">
+              <li>Current</li>
+              <li className="bg-gray-600 px-4 py-4 rounded-full text-zinc-50 md:text-3xl text-xl mt-2 md:w-64 w-48">{activeWords.current}</li>
+            </div>
+            <div className="text-center text-lime-500 md:text-xl text-base">
+              <li>Next</li>
+              <li className="md:text-2xl text-lg text-zinc-400 mt-2">{activeWords.next}</li>
+            </div>
+          </ul>
+          <div className="mt-10">
+            <input type="text" className="bg-transparent rounded-full border-2 border-lime-500 px-6 py-3 md:text-2xl text-lg focus:outline-none text-zinc-50 placeholder:text-zinc-400" placeholder="Start typing..." />
+            <p className="text-lime-500 text-center md:text-base text-sm mt-4">60 seconds left...</p>
           </div>
-          <div>
-            <h3>
-              Remaining Time: <span className="text-lime-500">60 seconds</span>{" "}
-            </h3>
-          </div>
-        </div>
-        <div className={`${roboto.className} text-gray-50 p-3 px-4 text-lg`}>
-          {textToWrite.map((word, i) => {
-            return (
-              <span key={i} className={`${writedText[i] === word ? "text-lime-500" : ""}`}>
-                {word}{" "}
-              </span>
-            );
-          })}
-          <input type="text" className="bg-transparent focus:outline-none" name="typeInput" placeholder="Start typing..." autoFocus />
-        </div>
-      </div>
+        </section>
+      )}
     </main>
   );
 }
