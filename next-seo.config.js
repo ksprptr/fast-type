9ac92709048527da83155472c20cf83636f040e2
Petr Kašpar <92054010+kasparrpetr@users.noreply.github.com>
2023-12-09 15:16:28 +0100

    added next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  defaultTitle: "FastType | Smart Typing Application",
+  description: "FastType is a smart typing application that helps you to improve your typing speed and accuracy.",
+  openGraph: {
+    type: "website",
+    url: "https://fasttype.kasparpetr.com/",
+    site_name: "FastType",
+    images: [
+      {
+        url: "https://cdn.kasparpetr.com/fasttype/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "FastType",
+      },
+    ],
+  },
+  twitter: {
+    cardType: "summary_large_image",
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
+import SEO from "../next-seo.config";
 import Footer from "@/layout/Footer";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 
 const inter = Inter({ style: "normal", subsets: ["latin-ext"], weight: ["300", "400", "500", "700"] });
@@ -13,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <div className={inter.className}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
       {!disabledPaths.includes(router.pathname) && <Footer />}
     </div>
