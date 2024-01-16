78916a1cc36281d0a710d68ec4c860fc59df05eb
Petr Kašpar <92054010+kasparrpetr@users.noreply.github.com>
2024-01-16 21:16:49 +0100

    updated year in footer

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="text-zic-50 text-center">
       <div className="flex flex-col py-4 text-lg text-gray-50">
-        <h3>&copy; FastType 2023</h3>
+        <h3>&copy; FastType {new Date().getFullYear()}</h3>
         <h3>
           Created by{" "}
           <Link href="https://kasparpetr.com" target="_blank" className="text-lime-500 hover:underline duration-150 font-medium">
